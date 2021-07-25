abbf348f9bd8e03f8a4f7674c1a492791d97eb7d
UserKazun <kazuusjlo8@gmail.com>
2021-07-25 16:25:50 +0900

    delete,putメソッドのAPIを実装

--- service/sampleData.go
+++ service/sampleData.go
@@ -26,3 +26,34 @@ func GetSampleData() ([]model.SampleData, error) {
 
 	return sampleData, err
 }
+
+func DeleteSampleData(sampleId string) error {
+	sampleData := []model.SampleData{}
+
+	err := db.Where("id = ?", sampleId).Delete(&sampleData).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func PutSampleData(sampleId string, name string) error {
+	beforeSampleData := model.SampleData{}
+	var err error
+
+	err = db.Where("id = ?", sampleId).First(&beforeSampleData).Error
+	if err != nil {
+		return err
+	}
+
+	afterSampleData := beforeSampleData
+	afterSampleData.Name = name
+
+	err = db.Model(&beforeSampleData).Update(&afterSampleData).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
